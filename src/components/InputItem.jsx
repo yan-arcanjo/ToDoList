6c3acecc135ed6e8dd062d836dc56a57eea554e7
Yan <yanarcanjo.dev@gmail.com>
2022-12-13 09:54:46 -0300

    Trabalhando com os checks

--- src/components/InputItem.jsx
+++ src/components/InputItem.jsx
@@ -1,12 +1,23 @@
+import { PlusCircle } from 'phosphor-react';
 import { useState } from 'react';
 import styles from './InputItem.module.css'
-import { PlusCircle } from 'phosphor-react'
 import { Item } from "./Item"
 
 export function InputItem () {
-    const [item, setItem] = useState([])
+    const [item, setItem] = useState([
+        'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'
+    ])
     const [newItemText, setNewItemText] = useState('')
+    const [checkCount, setCheckCount] = useState(0);
     //const itemsx = ['olá mundo', 'olá fernanda'];
+
+    function handleCheckCount () {
+        setCheckCount(checkCount + 1);
+    }
+
+    function handleUncheckCount () {
+        setCheckCount(checkCount - 1);
+    }
 
     function handleCreateNewItem(event) {
         event.preventDefault();
@@ -27,7 +38,7 @@ export function InputItem () {
     }
 
     return (
-        <div>
+        <div className={styles.main}>
             <form onSubmit={handleCreateNewItem} className={styles.inputBox}>
                 <div className={styles.addItem}>
                     <input 
@@ -39,20 +50,30 @@ export function InputItem () {
                     />
                     <button type='submit'>
                         Criar 
+                        <PlusCircle size={16}/>
                     </button>
+                    
                 </div>
              </form>
             <div className={styles.countItem}>
                 <h3>Tarefas criadas <span>{item.length}</span></h3>
-                <h3>Concluidas <span>0 de {item.length}</span></h3>
+                <h3>Concluidas <span>{checkCount} de {item.length}</span></h3>
             </div>
-    
+
+
              <div className={styles.wrapper}>
-                <main>
+                {item.length > 0 ? 
+                    <main>
                     {item.map(item => {
-                        return <Item content={item} OnDeleteItem={deleteItem}/>
+                        return <Item content={item} OnDeleteItem={deleteItem} onCheck={handleCheckCount} onUncheck = {handleUncheckCount}/>
                     })}
-                </main>
+                    </main> 
+                    : <main className={styles.noTasks}>
+                        <img src="../src/assets/Clipboard.svg" alt="" />
+                        <p><strong>Você ainda não tem tarefas cadastradas</strong><br></br>
+                            Crie tarefas e organize seus itens a fazer</p>
+                      </main>
+                }
              </div>
         </div>
        
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,40 +1,39 @@
 import styles from './Item.module.css'
-import {Trash, Check, X} from 'phosphor-react'
+import {Trash, Circle, CheckCircle, X} from 'phosphor-react'
 import { useState } from 'react'
 
-export function Item ({content, OnDeleteItem}) {
+export function Item ({content, OnDeleteItem, onCheck, onUncheck}) {
     const [checked, isChecked] = useState(false);
-    const [checkedOrNot, isCheckedOrNot] = useState(<Check size={26}/>)
+    const [checkedIcon, setCheckedIcon] = useState(<Circle color="var(--blue)" size={17.45}/>)
     
     function handleDeleteComment() {
-        OnDeleteItem(content)
+        OnDeleteItem(content);
+        if(checked!=false){
+            onUncheck();
+        }  
     }
 
+    
     return (
-        <div>
-            <div className={checked ? styles.boxItemChecked : styles.boxItem}>
-                <h2>{content}</h2> 
-            </div>
-
-        <div className={styles.options}>
-            <div className={styles.trash}>
-                <button onClick={handleDeleteComment}><Trash size={22}/></button> 
-            </div>
-
-            <div className={styles.check}>
-                <button className={styles.checkButton} onClick={() => {
+        <div className={styles.main}>
+            <div className={styles.boxItem}>
+                <button className={checked ? styles.checked : styles.checkButton} onClick={() => {
                     if(checked) {
                         isChecked(false);
-                        isCheckedOrNot(<Check size={26}/>)
+                        setCheckedIcon(<Circle color="var(--blue)" size={17.45}/>)
+                        onUncheck();
                     }else{
                         isChecked(true);
-                        isCheckedOrNot(<X color="red" size={26}/>)
+                        setCheckedIcon(<CheckCircle weight='fill' size={17.45} />)
+                        onCheck();
                     }
-                }}>{checkedOrNot}</button>
+                }}>{checkedIcon}</button>
 
-            </div>
-        </div>
-            
+                <p className={checked ? styles.textChecked : styles}>{content}</p> 
+                <div className={styles.trash}>
+                    <button onClick={handleDeleteComment}><Trash size={14}/></button> 
+                </div>
+            </div> 
         </div>
     )
 }
